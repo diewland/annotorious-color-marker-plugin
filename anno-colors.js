7e32a91574b12fdb78aa3c3dd3566ea905757ebe
diewland <diewland@gmail.com>
2017-10-12 16:16:56 +0700

    revise to anno plugin

--- anno-colors.js
+++ anno-colors.js
@@ -1,23 +1,23 @@
-window.AnnoColors = function(img_sel, color_list){
-  
+annotorious.plugin.ColorPlugin = function(options) {
+  this.img_id     = options.img_id;
+  this.color_list = options.color_list;
+  this.controller = options.controller === false ? false : true;
+}
+
+annotorious.plugin.ColorPlugin.prototype.initPlugin = function(anno) {
   var that = this;
-  var cur_color = color_list[0].color;
-  var img_dom = document.querySelector(img_sel);
-  window.colors = [];
+  var cur_color = this.color_list[0].color;
+  var img_dom = document.getElementById(this.img_id);
+  var colors = [];
   var color2label = {};
 
   // update color values
-  color_list.forEach(function(m){
+  this.color_list.forEach(function(m){
     colors.push(m.color);
     color2label[m.color] = m.label;
   });
 
   // utility function(s)
-  this._reset_buttons = function(){
-    document.querySelectorAll('._color_marker').forEach(function(btn){
-      btn.disabled = false;
-    });
-  };
   this._find_anno = function(evt){
     var annos = anno.getAnnotations();
     for(var i=0; i<annos.length; i++){
@@ -43,21 +43,23 @@ window.AnnoColors = function(img_sel, color_list){
   };
 
   // render selector buttons
-  var html = '';
-  color_list.forEach(function(marker){
-    html += "<input class='_color_marker' style='background-color: "+ marker.color +";' type='button' value='"+ marker.label +"' /> ";
-  });
-  img_dom.parentNode.insertAdjacentHTML('beforebegin', html + '<br>');
-  document.querySelectorAll('._color_marker').forEach(function(btn){
-    (function(btn){
-      btn.onclick = function(){
-        cur_color = btn.style.backgroundColor;
-        that._reset_buttons();
-        btn.disabled = true;
-      }
-    })(btn);
-  });
-  document.querySelectorAll('._color_marker')[0].click();
+  if(this.controller){
+    var html = '';
+    this.color_list.forEach(function(marker){
+      html += "<input class='_color_marker' style='background-color: "+ marker.color +";' type='button' value='"+ marker.label +"' /> ";
+    });
+    img_dom.parentNode.insertAdjacentHTML('beforebegin', html + '<br>');
+    document.querySelectorAll('._color_marker').forEach(function(btn){
+      (function(btn){
+        btn.onclick = function(){
+          cur_color = btn.style.backgroundColor;
+          document.querySelectorAll('._color_marker').forEach(function(btn){ btn.disabled = false; });
+          btn.disabled = true;
+        }
+      })(btn);
+    });
+    document.querySelectorAll('._color_marker')[0].click();
+  }
 
   // hook colorize in onSelectionCompleted event
   anno.addHandler('onSelectionCompleted', function(evt){
@@ -71,8 +73,10 @@ window.AnnoColors = function(img_sel, color_list){
       hi_stroke: cur_color,
     };
     // re-draw shape
-    anno.addAnnotation({ src: img_dom.src,
+    anno.addAnnotation({
+      src: img_dom.src,
       shapes: [ shape ],
+      text: '',
     })     
   });
 
@@ -88,7 +92,7 @@ window.AnnoColors = function(img_sel, color_list){
   });
 
   // add annotations by color
-  this.addAnnotation = function(color, an){
+  anno.addColorAnnotation = function(color, an){
     an.shapes[0].style = {
       outline:   color,
       stroke:    color,
@@ -98,9 +102,9 @@ window.AnnoColors = function(img_sel, color_list){
   };
 
   // get annotations by color
-  this.getAnnotations = function(){
+  anno.getColorAnnotations = function(){
     var annos = {};
-    color_list.forEach(function(m){
+    that.color_list.forEach(function(m){
       annos[m.label] = [];
     });
     anno.getAnnotations().forEach(function(an){
